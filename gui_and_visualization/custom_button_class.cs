a7153ff6330e24d6a1516601e11e91f6a4a22e64
sylatupa <sylatupa@asu.edu>
2014-10-29 13:07:49 -0700

    added custom button and grid class
    i can explain what these are

---
+++ gui_and_visualization/custom_button_class.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Controls;
+
+namespace gui_and_visualization
+{
+    class custom_button_class
+    {
+    }
+    public class custom_button : System.Windows.Forms.Button
+    {
+         public static void main_button() {
+         int x; //button x position
+         int y; //button y position
+         bool has_dot; //pattern
+         bool has_dot_above;
+         bool has_dot_left;
+         bool has_dot_right;
+         bool has_dot_below;
+         Random r = new Random();
+         int random = r.Next(1, 2);       
+            if (random == 1)
+            {
+                has_dot = true;
+            }
+            else { has_dot = false; }
+        
+        }
+
+
+
+        public List<string> GetAllPropertyValues()
+        {
+            List<string> values = new List<string>();
+            foreach (var pi in typeof(custom_button).GetProperties())
+            {
+                values.Add(pi.GetValue(this, null).ToString());
+            }
+
+            return values;
+        }
+    }
+}
---
+++ gui_and_visualization/grid_class.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace gui_and_visualization
+{
+    class grid_class
+    {
+        public static int grid_size = 10;
+        //List<string> values = new List<string>();
+        custom_button[,] buttonArray = new custom_button[grid_size, grid_size]; //{ { 1, 2 }, { 3, 4 }, { 5, 6 }, { 7, 8 } };
+
+
+        public void main_grid () {
+
+            // 1) Build Grid using custom button class and setting the pattern
+            for (int i = 0; i <= grid_size; i++)
+            {
+                custom_button button = new custom_button();
+                //button.main_button();
+            }
+
+        }
+
+    }
+}
