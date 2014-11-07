e3c8424bb05d7af52120e06e1dbca502847c4606
sylatupa <sylatupa@asu.edu>
2014-11-07 15:07:31 -0700

    good stuff for functionality
    working code as usual

--- start/MainWindow.xaml.cs
+++ start/MainWindow.xaml.cs
@@ -15,7 +15,8 @@ using gui_and_visualization;
 //using main; //TI main functions
 namespace Working_Memory_Battery_and_Sensor_Input
 {
-    class cooridinate
+
+    public class cooridinate
     {
         public int x;
         public int y;
@@ -28,36 +29,54 @@ namespace Working_Memory_Battery_and_Sensor_Input
         /// This calls main
         /// </summary>
         /// 
-        custom_button[,] buttonArray;
-        cooridinate[] random_pattern;
-        cooridinate[] random_user_pattern;
-
+        public custom_button[,] buttonArray;
+        public cooridinate[] random_pattern;
+        public cooridinate[] random_user_pattern;
+        Random num = new Random();
+        Random number = new Random();
         string current_player1 = "", current_player2 = "";
         int p1 = 0, p2 = 0;
 
         public MainWindow()
         {
+            int size = 10;
+            int number_dots = 5;
             InitializeComponent();
             Main main = new Main();
             main.main();
             Console.WriteLine("Starting Main");
-            set_button_grid(10);
-            get_random_pattern(10, 5);
-            set_random_pattern();
-            set_user_random_pattern();
+            custom_button[,] button_array = set_button_grid(size);
+            random_pattern = get_random_pattern(size, number_dots);
+            random_user_pattern = get_random_pattern(size, number_dots);
+            set_random_pattern(random_pattern, "1");
+            set_random_pattern(random_user_pattern, "2");
+            set_bar_movement();
+            Dictionary<string, int[]> dictionary = new Dictionary<string, int[]>();
+            //{pleasure, arrousal, dominace};
+            int[] engagement = { 1, 1, 1 };
+            int[] boredom = { -1, -1, -1 };
+            int[] frustration = { -1, 1, -1 };
+            int[] meditation = { 1, -1, 1 };
+            int[] agreement = { 1, -1, -1 };
+            int[] concentrating = { 1, -1, 1 };
+            int[] disagreement = { -1, 1, 1 };
+            int[] interested = { 1, 1, -1 };
+
+            dictionary.Add("engagement", engagement);
+            dictionary.Add("boredom", boredom);
+            dictionary.Add("frustration", frustration);
+            dictionary.Add("meditation", meditation);
+            dictionary.Add("agreement", agreement);
+            dictionary.Add("concentrating", concentrating);
+            dictionary.Add("disagreement", disagreement);
+            dictionary.Add("interested", interested);
         }
         private void HyperlinkButton_Click(object sender, RoutedEventArgs e)
         {
-            NavigationService nav = NavigationService.GetNavigationService(this);
-            Console.WriteLine(nav.ToString());
-            //nav.Navigate(new Uri("Introduction.xaml", UriKind.RelativeOrAbsolute));           // NavigationService n = new NavigationService();
-            //n.Navigate(new Uri("SelectionPage.xaml", UriKind.Relative));
-            //n.Navigate(new Uri("Introduction.xaml", UriKind.Relative));
-            //n.Navigate(typeof(Introduction));
-
+            grid_button.Visibility = Visibility.Hidden;
         }
 
-        private void set_button_grid(int size)
+        private custom_button[,] set_button_grid(int size)
         {
             Console.WriteLine("Starting set button grid");
             buttonArray = new custom_button[size, size];
@@ -66,14 +85,12 @@ namespace Working_Memory_Battery_and_Sensor_Input
                 for (int j = 0; j < size; j++)
                 {
                     custom_button button1 = new custom_button();
-
                     button1.Height = 45;
                     button1.Width = 45;
                     button1.Background = Brushes.CornflowerBlue;
                     button1.Content = 'X';
                     button1.Name = "button" + i.ToString() + "button" + j.ToString();
-
-                    button1.Content = i.ToString() + "," + j.ToString();
+                    //button1.Content = i.ToString() + "," + j.ToString();
                     button1.x = i;
                     button1.y = j;
                     buttonArray[i, j] = button1;
@@ -85,54 +102,67 @@ namespace Working_Memory_Battery_and_Sensor_Input
                 {
                     Console.WriteLine("i: " + i + " j: " + j);
                     Console.WriteLine("row: " + buttonArray[i, j].x + " col: " + buttonArray[i, j].y);
-
-                    //stackpanel_button.Children.Add(buttonArray[i, j]);
-
                     buttonArray[i, j].SetValue(Grid.RowProperty, buttonArray[i, j].x);
                     buttonArray[i, j].SetValue(Grid.ColumnProperty, buttonArray[i, j].y);
-
                     grid_button.Children.Add(buttonArray[i, j]);
-
                     Grid.SetRow(buttonArray[i, j], buttonArray[i, j].x);
                     Grid.SetColumn(buttonArray[i, j], buttonArray[i, j].y);
                 }
             }
+            return buttonArray;
         }
-
-        Random number;
-
         private cooridinate[] get_random_pattern(int size, int number_dots)
         {
-            number = new Random();
-
-            random_pattern = new cooridinate[number_dots];
-            random_user_pattern = new cooridinate[number_dots];
+            
+            cooridinate[] dot_pattern = new cooridinate[number_dots];
             for (int i = 0; i < number_dots; i++)
             {
                 cooridinate coord = new cooridinate();
                 cooridinate coord2 = new cooridinate();
-                coord.x = (int)number.Next(0, size);
-                coord.y = (int)number.Next(0, size);
-                random_pattern[i] = coord;
-                coord2.x = (int)number.Next(0, size);
-                coord2.y = (int)number.Next(0, size);
-                random_user_pattern[i] = coord2;
+                coord.x = (int)num.Next(0, size);
+                coord.y = (int)num.Next(0, size);
+                dot_pattern[i] = coord;
             }
-            return random_pattern;
+            
+            return dot_pattern;
         }
-        public void set_random_pattern()
+        public void set_random_pattern(cooridinate[] coords, string color)
         {
-            for (int i = 0; i < random_pattern.Length; i++)
+           if (color == "1") {
+            for (int i = 0; i < coords.Length; i++)
             {
-                buttonArray[random_pattern[i].x, random_pattern[i].y].Background = Brushes.Yellow;
+                buttonArray[coords[i].x, coords[i].y].Background = Brushes.Green;
+            }
+           }
+           if (color == "2") {
+            for (int i = 0; i < coords.Length; i++)
+            {
+                buttonArray[coords[i].x, coords[i].y].Background = Brushes.Yellow;
+            }
+           }
+        }
+        public async void set_bar_movement()
+        {
+            
+            for (int i = 0; i < 100; i++)
+            {
+                await Task.Delay(700);
+                bar_Attention.Height = (int)number.Next(0, 100);
+                bar_Dominance.Height = (int)number.Next(0, 100);
+                bar_Pleasure.Height = (int)number.Next(0, 100);
             }
         }
-        public void set_user_random_pattern()
+        public void show_bar_graph(object sender, RoutedEventArgs e)
         {
-            for (int i = 0; i < random_user_pattern.Length; i++)
+            if (grid_bar_graph.Visibility == Visibility.Hidden)
             {
-                buttonArray[random_user_pattern[i].x, random_user_pattern[i].y].Background = Brushes.Green;
+                grid_bar_graph.Visibility = Visibility.Visible;
             }
+            else
+            {
+                grid_bar_graph.Visibility = Visibility.Hidden;
+            }
+
         }
     }
 
