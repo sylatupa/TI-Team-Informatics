b533f109982c927bd55735fc418933a1edca4ace
sylatupa <sylatupa@asu.edu>
2015-02-05 14:33:23 -0700

    doing commit for you sucka
    more goodies

--- start/emotiv.cs
+++ start/emotiv.cs
@@ -10,28 +10,45 @@ namespace Working_Memory_Battery_and_Sensor_Input
 {
     class emotiv
     {
-            string hostname= "localhost";
-    int port = 7575;
+        string hostname = "localhost";
+        int port = 7575;
         public void get_tcp()
         {
             //connect to server and get intput/output stream
-           /* TcpClient client = new TcpClient(hostname, port);
+            //TcpClient client = new TcpClient(hostname, port);
             IPAddress ipAd = IPAddress.Parse("127.0.0.1");
-            TcpListener myList = new TcpListener(ipAd, 7575);
-            myList.Start();
-            Socket s = myList.AcceptSocket();
-            * */
-           // Console.WriteLine("Connection accepted from " + s.RemoteEndPoint);
-            Console.WriteLine("Connection accepted from " );
+            TcpListener myList = new TcpListener(ipAd, 7574);
+            //myList.Start();
+            
+            //Socket s = myList.AcceptSocket();
+            // Console.WriteLine("Connection accepted from " + s.RemoteEndPoint);
+            Console.WriteLine("Connection accepted from ");
+            Console.WriteLine("Connection accepted from ");
+            Console.WriteLine("Connection accepted from ");
+
+            while (true)
+            {
+                //TcpClient client = myList.AcceptTcpClient();
+                TcpClient tcpClient = myList.AcceptTcpClient();
+                // Read the data stream from the client. 
+                byte[] bytes = new byte[256];
+                NetworkStream stream = tcpClient.GetStream();
+                stream.Read(bytes, 0, bytes.Length);
+                SocketHelper helper = new SocketHelper();
+                helper.processMsg(tcpClient, stream, bytes);
+
+
+            }
+            
             /*
             byte[] b = new byte[100];
             int k = s.Receive(b);
             Console.WriteLine("Recieved...");
             for (int i = 0; i < k; i++)
                 Console.Write(Convert.ToChar(b[i]));
-             */
-
-   }
+             * 
+             */ 
+        }
 
     }
 }
