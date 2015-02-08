6013c2e914e4103a4981ee49b12d9e68a62be2ef
sylatupa <sylatupa@asu.edu>
2015-02-08 13:36:50 -0700

    commit for nick
    in the repo for the socket

--- start/MainWindow.xaml.cs
+++ start/MainWindow.xaml.cs
@@ -11,6 +11,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using System.Threading;
 //using main; //TI main functions
 namespace Working_Memory_Battery_and_Sensor_Input
 {
@@ -29,7 +30,8 @@ namespace Working_Memory_Battery_and_Sensor_Input
         {
             InitializeComponent();
             Main main = new Main();
-            emotiv.get_tcp();
+            //emotiv.get_tcp();
+            emotiv.StartListening();
             main.main();
             //Console.WriteLine("Starting Main");
             set_button_navigation(this, new RoutedEventArgs());
--- start/emotiv.cs
+++ start/emotiv.cs
@@ -4,7 +4,8 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Net.Sockets;
-using System.Net;  //TI added
+using System.Net;
+using System.Threading;  //TI added
 
 namespace Working_Memory_Battery_and_Sensor_Input
 {
@@ -12,43 +13,83 @@ namespace Working_Memory_Battery_and_Sensor_Input
     {
         string hostname = "localhost";
         int port = 7575;
+        public Socket workSocket = null;
+        public const int BufferSize = 1024;
+        public byte[] buffer = new byte[BufferSize];
+        public StringBuilder sb = new StringBuilder();
+ 
         public void get_tcp()
         {
-            //connect to server and get intput/output stream
-            //TcpClient client = new TcpClient(hostname, port);
             IPAddress ipAd = IPAddress.Parse("127.0.0.1");
-            TcpListener myList = new TcpListener(ipAd, 7574);
-            //myList.Start();
-            
-            //Socket s = myList.AcceptSocket();
-            // Console.WriteLine("Connection accepted from " + s.RemoteEndPoint);
-            Console.WriteLine("Connection accepted from ");
-            Console.WriteLine("Connection accepted from ");
-            Console.WriteLine("Connection accepted from ");
-
+            TcpListener myList = new TcpListener(ipAd, 7474);
+            //IPEndPoint localEndPoint = new IPEndPoint(ipAddress, 11000);
+            myList.Start();
             while (true)
             {
-                //TcpClient client = myList.AcceptTcpClient();
-                TcpClient tcpClient = myList.AcceptTcpClient();
-                // Read the data stream from the client. 
+                Console.WriteLine("here");
+                Socket socket = new Socket(AddressFamily.InterNetwork,SocketType.Stream, ProtocolType.Tcp);
+                socket.Bind(ipAd);
+                socket.Listen(myList);
+                //TcpClient tcpClient = myList.AcceptTcpClient();
+                Console.WriteLine(socket.ToString());
                 byte[] bytes = new byte[256];
-                NetworkStream stream = tcpClient.GetStream();
-                stream.Read(bytes, 0, bytes.Length);
-                SocketHelper helper = new SocketHelper();
-                helper.processMsg(tcpClient, stream, bytes);
-
-
+                Console.WriteLine("here");
+                //NetworkStream stream = tcpClient.GetStream();
+                //stream.Read(bytes, 0, bytes.Length);
+                Console.WriteLine(bytes);
             }
-            
-            /*
-            byte[] b = new byte[100];
-            int k = s.Receive(b);
-            Console.WriteLine("Recieved...");
-            for (int i = 0; i < k; i++)
-                Console.Write(Convert.ToChar(b[i]));
-             * 
-             */ 
         }
-
+ 
     }
 }
+/*
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.ObjectInputStream;
+import java.net.InetAddress;
+import java.net.Socket;
+
+public class TCPclient {
+    public static void main(String[] args) throws IOException {
+
+    Socket client = null;
+    BufferedReader input = null;
+    stop = false;
+    Object measureLocal;
+    try {  
+      client = new Socket(InetAddress.getByName(Ip), port);
+      input = new BufferedReader(new InputStreamReader(client.getInputStream()));
+      client.setSoTimeout(SO_TIMEOUT);
+    } catch (Exception ex) {
+      stop = true;
+    }
+    // loop
+    while (!stop) {
+      try {
+        //
+        measureLocal = input.readLine();
+        //
+      } catch (Exception sce) {
+        break;
+      }
+      if (measureLocal == null) {
+        stop = true;
+      } else { 
+         // HERE YOU CAN DO SOMETHING WITH THE VALUE….
+      }
+      try {
+        Thread.sleep(delay);
+      } catch (Exception ex) {
+      }
+    }
+    try {      
+      if (input!=null) input.close();
+      if (client!=null) client.close();
+    } catch (Exception e) {  
+
+    } 
+    
+  }
+}
+*/
